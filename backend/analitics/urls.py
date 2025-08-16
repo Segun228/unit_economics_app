4e7c3e2bb047ec7870a0e9c9d526b046c9d9cb0e
Segun228 <segunperkele@gmail.com>
2025-08-16 23:17:11 +0300

    adding urls handlers

--- backend/analitics/urls.py
+++ backend/analitics/urls.py
@@ -1,28 +1,28 @@
 from django.urls import path
-
+from . import views
 
 urlpatterns = [
-    path('report/unit/<int:unit_id>/text/', view , name='generate-report-file'),
-    path('report/set/<int:set_id>/text/', view , name='generate-report-file'),
-    path('report/set/<int:set_id>/text/', view , name='generate-report-file'),
-    
-    path('report/unit/<int:unit_id>/xlsx/', view , name='generate-report-file'),
-    path('report/set/<int:set_id>/xlsx/', view , name='generate-report-file'),
-    path('report/set/<int:set_id>/xlsx/', view , name='generate-report-file'),
+    # ---------------- REPORT ----------------
+    path('report/unit/<int:unit_id>/text/', views.UnitTextReportView.as_view(), name='report_unit_text'),
+    path('report/set/<int:set_id>/text/', views.SetTextReportView.as_view(), name='report_set_text'),
 
-    path('report/unit/<int:unit_id>/image/', view , name='generate-report-file'),
-    path('report/set/<int:set_id>/image/', view , name='generate-report-file'),
-    path('report/set/<int:set_id>/image/', view , name='generate-report-file'),
+    path('report/unit/<int:unit_id>/xlsx/', views.UnitExelReportView.as_view(), name='report_unit_xlsx'),
+    path('report/set/<int:set_id>/xlsx/', views.SetExelReportView.as_view(), name='report_set_xlsx'),
 
-    path('evaluate/unit/<int:unit_id>/ущзшщшакш_point', view , name='generate-report-file'),
-    path('evaluate/unit/<int:unit_id>/required_investments', view , name='generate-report-file'),
-    path('evaluate/unit/<int:unit_id>/expected_profit', view , name='generate-report-file'),
-    
-    path('evaluate/set/<int:set_id>/ущзшщшакш_point', view , name='generate-report-file'),
-    path('evaluate/set/<int:set_id>/required_investments', view , name='generate-report-file'),
-    path('evaluate/set/<int:set_id>/expected_profit', view , name='generate-report-file'),
+    path('report/unit/<int:unit_id>/image/', views.UnitImageReportView.as_view(), name='report_unit_image'),
+    path('report/set/<int:set_id>/image/', views.SetImageReportView.as_view(), name='report_set_image'),
 
-    path('get_kpi/unit/<int:unit_id>/ущзшщшакш_point', view , name='generate-report-file'),
-    path('get_kpi/unit/<int:unit_id>/required_investments', view , name='generate-report-file'),
-    path('get_kpi/unit/<int:unit_id>/expected_profit', view , name='generate-report-file'),
+    # ---------------- EVALUATE ----------------
+    path('evaluate/unit/<int:unit_id>/break_even_point/', views.UnitCountBEPView.as_view(), name='evaluate_unit_bep'),
+    path('evaluate/unit/<int:unit_id>/required_investments/', views.UnitCountRIView.as_view(), name='evaluate_unit_ri'),
+    path('evaluate/unit/<int:unit_id>/expected_profit/', views.UnitCountEPView.as_view(), name='evaluate_unit_ep'),
+
+    path('evaluate/set/<int:set_id>/break_even_point/', views.SetCountBEPView.as_view(), name='evaluate_set_bep'),
+    path('evaluate/set/<int:set_id>/required_investments/', views.SetCountRIView.as_view(), name='evaluate_set_ri'),
+    path('evaluate/set/<int:set_id>/expected_profit/', views.SetCountEPView.as_view(), name='evaluate_set_ep'),
+
+    # ---------------- GET KPI ----------------
+    path('get_kpi/unit/<int:unit_id>/break_even_point/', views.UnitKPICountBEPView.as_view(), name='kpi_unit_bep'),
+    path('get_kpi/unit/<int:unit_id>/required_investments/', views.UnitKPICountRIView.as_view(), name='kpi_unit_ri'),
+    path('get_kpi/unit/<int:unit_id>/expected_profit/', views.UnitKPICountEPView.as_view(), name='kpi_unit_ep'),
 ]
--- backend/analitics/views.py
+++ backend/analitics/views.py
@@ -1,3 +1,167 @@
-from django.shortcuts import render
+from rest_framework.generics import (
+    ListCreateAPIView, RetrieveUpdateDestroyAPIView
+)
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
-# Create your views here.
+from api.models import ModelSet, UnitModel
+
+from api.serializers import (
+    UnitModelSerializer,
+    ModelSetReadSerializer
+)
+
+from backend.authentication import TelegramAuthentication
+
+from rest_framework.response import Response
+
+
+class UnitTextReportView(ListCreateAPIView):
+    def post(self, request, *args, **kwargs):
+        unit_id = kwargs.get("unit_id")
+        try:
+            unit = UnitModel.objects.get(pk=unit_id)
+        except UnitModel.DoesNotExist:
+            return Response({"error": "Unit not found"}, status=404)
+        data = UnitModelSerializer(unit).data
+
+
+class SetTextReportView(ListCreateAPIView):
+    def post(self, request, *args, **kwargs):
+        set_id = kwargs.get("set_id")
+        try:
+            set = ModelSet.objects.get(pk=set_id)
+        except ModelSet.DoesNotExist:
+            return Response({"error": "Unit not found"}, status=404)
+        data = UnitModelSerializer(set).data
+
+
+class UnitExelReportView(ListCreateAPIView):
+    def post(self, request, *args, **kwargs):
+        unit_id = kwargs.get("unit_id")
+        try:
+            unit = UnitModel.objects.get(pk=unit_id)
+        except UnitModel.DoesNotExist:
+            return Response({"error": "Unit not found"}, status=404)
+        data = UnitModelSerializer(unit).data
+
+
+class SetExelReportView(ListCreateAPIView):
+    def post(self, request, *args, **kwargs):
+        set_id = kwargs.get("set_id")
+        try:
+            set = ModelSet.objects.get(pk=set_id)
+        except ModelSet.DoesNotExist:
+            return Response({"error": "Unit not found"}, status=404)
+        data = UnitModelSerializer(set).data
+
+
+class UnitImageReportView(ListCreateAPIView):
+    def post(self, request, *args, **kwargs):
+        unit_id = kwargs.get("unit_id")
+        try:
+            unit = UnitModel.objects.get(pk=unit_id)
+        except UnitModel.DoesNotExist:
+            return Response({"error": "Unit not found"}, status=404)
+        data = UnitModelSerializer(unit).data
+
+
+class SetImageReportView(ListCreateAPIView):
+    def post(self, request, *args, **kwargs):
+        set_id = kwargs.get("set_id")
+        try:
+            set = ModelSet.objects.get(pk=set_id)
+        except ModelSet.DoesNotExist:
+            return Response({"error": "Unit not found"}, status=404)
+        data = UnitModelSerializer(set).data
+
+
+class UnitCountBEPView(ListCreateAPIView):
+    def post(self, request, *args, **kwargs):
+        unit_id = kwargs.get("unit_id")
+        try:
+            unit = UnitModel.objects.get(pk=unit_id)
+        except UnitModel.DoesNotExist:
+            return Response({"error": "Unit not found"}, status=404)
+        data = UnitModelSerializer(unit).data
+
+
+class UnitCountRIView(ListCreateAPIView):
+    def post(self, request, *args, **kwargs):
+        unit_id = kwargs.get("unit_id")
+        try:
+            unit = UnitModel.objects.get(pk=unit_id)
+        except UnitModel.DoesNotExist:
+            return Response({"error": "Unit not found"}, status=404)
+        data = UnitModelSerializer(unit).data
+
+
+class UnitCountEPView(ListCreateAPIView):
+    def post(self, request, *args, **kwargs):
+        unit_id = kwargs.get("unit_id")
+        try:
+            unit = UnitModel.objects.get(pk=unit_id)
+        except UnitModel.DoesNotExist:
+            return Response({"error": "Unit not found"}, status=404)
+        data = UnitModelSerializer(unit).data
+
+
+class SetCountBEPView(ListCreateAPIView):
+    def post(self, request, *args, **kwargs):
+        set_id = kwargs.get("set_id")
+        try:
+            set = ModelSet.objects.get(pk=set_id)
+        except ModelSet.DoesNotExist:
+            return Response({"error": "Unit not found"}, status=404)
+        data = UnitModelSerializer(set).data
+
+
+class SetCountRIView(ListCreateAPIView):
+    def post(self, request, *args, **kwargs):
+        set_id = kwargs.get("set_id")
+        try:
+            set = ModelSet.objects.get(pk=set_id)
+        except ModelSet.DoesNotExist:
+            return Response({"error": "Unit not found"}, status=404)
+        data = UnitModelSerializer(set).data
+
+
+class SetCountEPView(ListCreateAPIView):
+    def post(self, request, *args, **kwargs):
+        set_id = kwargs.get("set_id")
+        try:
+            set = ModelSet.objects.get(pk=set_id)
+        except ModelSet.DoesNotExist:
+            return Response({"error": "Unit not found"}, status=404)
+        data = UnitModelSerializer(set).data
+
+
+class UnitKPICountBEPView(ListCreateAPIView):
+    def post(self, request, *args, **kwargs):
+        unit_id = kwargs.get("unit_id")
+        try:
+            unit = UnitModel.objects.get(pk=unit_id)
+        except UnitModel.DoesNotExist:
+            return Response({"error": "Unit not found"}, status=404)
+        data = UnitModelSerializer(unit).data
+
+
+class UnitKPICountRIView(ListCreateAPIView):
+    def post(self, request, *args, **kwargs):
+        unit_id = kwargs.get("unit_id")
+        try:
+            unit = UnitModel.objects.get(pk=unit_id)
+        except UnitModel.DoesNotExist:
+            return Response({"error": "Unit not found"}, status=404)
+        data = UnitModelSerializer(unit).data
+
+
+class UnitKPICountEPView(ListCreateAPIView):
+    def post(self, request, *args, **kwargs):
+        unit_id = kwargs.get("unit_id")
+        try:
+            unit = UnitModel.objects.get(pk=unit_id)
+        except UnitModel.DoesNotExist:
+            return Response({"error": "Unit not found"}, status=404)
+        data = UnitModelSerializer(unit).data
+
+
